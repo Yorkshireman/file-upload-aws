26e99a5738307262b9a6cbcb72de7fa27bbba41d
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-01-27 21:28:20 +0000

    enable file upload

--- handler.js
+++ handler.js
@@ -1,15 +1,26 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hello!',
-        event,
-      },
-      null,
-      2
-    ),
+var AWS = require('aws-sdk');
+
+module.exports.requestUploadURL = (event, _, callback) => {
+  var s3 = new AWS.S3();
+  var params = JSON.parse(event.body);
+
+  var s3Params = {
+    Bucket: 'file-upload-aws',
+    Key: params.name,
+    ContentType: params.type,
+    ACL: 'public-read',
   };
-};
+
+  var uploadURL = s3.getSignedUrl('putObject', s3Params);
+
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      // 'Access-Control-Allow-Origin': 'https://frosty-morse-b7b3c9.netlify.com'
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({ uploadURL: uploadURL }),
+  })
+}
